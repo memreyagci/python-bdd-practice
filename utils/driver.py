cfe332b66373aabea2f62785240bd2e12cb70e8a
Musa Emre Yagci <emre@meyagci.com>
2022-07-10 06:59:26 +0800

    Driver util is updated.
    You can specify the browser in config.properties under selenium/browser
    now. Supported keywords: firefox, chrome, and edge

--- utils/driver.py
+++ utils/driver.py
@@ -1,6 +1,14 @@
 from selenium import webdriver
+
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+from utils.config_reader import ConfigReader
 
 
 class Driver:
@@ -9,17 +17,37 @@ class Driver:
         self.driver = None
 
     def get_driver(self) -> webdriver:
+        browser = ConfigReader().get_value("selenium", "browser")
+
         if self.driver is None:
-            firefox_options = FirefoxOptions()
-            firefox_options.add_argument("--detach")
+            if browser == "firefox":
+                firefox_options = FirefoxOptions()
+                firefox_options.add_argument("--detach")
 
-            self.driver = webdriver.Firefox(
-                executable_path=GeckoDriverManager().install(),
-                options=firefox_options)
-            self.driver.maximize_window()
+                self.driver = webdriver.Firefox(
+                    executable_path=GeckoDriverManager().install(),
+                    options=firefox_options)
 
+            elif browser == "chrome":
+                chrome_options = ChromeOptions()
+                chrome_options.add_argument("--detach")
+
+                self.driver = webdriver.Firefox(
+                    executable_path=ChromeDriverManager().install(),
+                    options=chrome_options)
+
+            elif browser == "edge":
+                edge_options = EdgeOptions()
+                edge_options.add_argument("--detach")
+
+                self.driver = webdriver.Edge(
+                    executable_path=EdgeChromiumDriverManager().install(),
+                    options=edge_options)
+
+        self.driver.maximize_window()
         return self.driver
 
     def close_driver(self):
         if self.driver is not None:
             self.driver.close()
+            self.driver = None
